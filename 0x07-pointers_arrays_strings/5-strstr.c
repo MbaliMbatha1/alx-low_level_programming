03698ef4fafd190a868c08262fa8e36a09ad04d6
MbaliMbatha1 <sibusisiwe.mbatha4@gmail.com>
2023-03-06 12:22:14 -0800

    a function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
- * _strstr - returns rest of a string beginning from 1st instance of a substr
- * @haystack: string to scan
- * @needle: string to match
+ * main - check the code
  *
- * Return: see function description above, NULL is substr not found
+ * Return: Always 0.
  */
-char *_strstr(char *haystack, char *needle)
+int main(void)
 {
-	int i, j, c, nl = 0;
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-	while (*(needle + nl))
-		nl++;
-
-	for (i = 0; *(haystack + i); i++)
-	{
-		c = i;
-		for (j = 0; *(needle + j); j++)
-		{
-			if (*(haystack + c) == *(needle + j))
-			{
-				if (j == nl - 1)
-					return (haystack + c - j);
-				c++;
-			}
-			else
-			{
-				break;
-			}
-		}
-	}
-
-	return ('\0');
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
