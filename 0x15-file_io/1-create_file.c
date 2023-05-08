82c40be3d38cb964572b1421acb8c953670e64a2
MbaliMbatha1 <124329266+MbaliMbatha1@users.noreply.github.com>
2023-05-09 01:31:06 +0200

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
-* create_file - createss a file 
-* @filename: filename.
-* @text_content: content written in the file
-*
-* Return: 1 if is a success. -1 if it fails.
-*/
-int create_file(consst char *filename. char *text_content)
+ * create_file - this funciton creates a file
+ * @filename: this will be the name of the file.
+ * @text_content: this will be what is written in the file.
+ *
+ * Return: 1 if success else -1 
+ */
+int create_file(const char *filename, char *text_content)
 {
-        int gf;
-        int nletters;
-        int yty;
-  
-        if (!filenaame)
-                  return (-1);
-         
-        gf = open( filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-  
-       if (gf == -1)
-               return (-1);
-  
-       if (!text_content)
-                text_content = " ";
-  
-       for (nletters =0; text_content[nletters, nletters);
-                ;
-        yty = wtite(gf, text_content, nletters);
-                                      
-                                      
-        if (yty == -1) 
-                return (-1)
-                                      
-        close(gf);  
-                        
-        return (1);                               
- } 
+	int gf;
+	int conlatter;
+	int yty;
+
+	if (!filename)
+		return (-1);
+
+	gf = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (gf == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (conlatter = 0; text_content[conlatter]; conlatter++)
+		;
+
+	yty = write(gf, text_content, conlatter);
+
+	if (yty == -1)
+		return (-1);
+
+	close(gf);
+
+	return (1);
+}
